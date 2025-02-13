99e5cdbeea7c476cad02e75bf308484019b9b1bb
yuntian <yuntian@tsign.cn>
2025-02-13 11:01:04 +0800

    feat: 脚本

--- prisma/create_database.sql
+++ prisma/create_database.sql
@@ -1,16 +1,2 @@
 create database if not exists fe_cloud_shadow;
 create database if not exists fe_cloud;
-
-create table if not exists user;
-create table if not exists role;
-
-TRUNCATE TABLE role;
-
-INSERT INTO `role`(`name`, `code`) VALUES
-('超级管理员', 1),
-('管理员', 2),
-('普通用户', 3),
-('访客用户', 4)
-;
-
-insert into `user`(`name`, `password`, `roleId`) values('admin','e1ea532cc067acedc149d39a87716f3f', 1) ON DUPLICATE KEY UPDATE roleId=roleId;
--- prisma/init.sql
+++ prisma/init.sql
@@ -7,4 +7,14 @@ INSERT INTO `role`(`name`, `code`) VALUES
 ('访客用户', 4)
 ;
 
-insert into `user`(`name`, `password`, `roleId`) values('admin','e1ea532cc067acedc149d39a87716f3f', 1) ON DUPLICATE KEY UPDATE roleId=roleId;
+insert into `user`(`name`, `password`, `roleId`) 
+select 'admin','e1ea532cc067acedc149d39a87716f3f',1 FROM dual 
+where not exists (
+  select roleId from user where roleId = 1
+);
+
+insert into `user`(`name`, `password`, `roleId`) 
+select '张三','a3aa6035b15bb6ea4041a939c315914a',3 FROM dual 
+where not exists (
+  select name from user where name = '张三'
+);
