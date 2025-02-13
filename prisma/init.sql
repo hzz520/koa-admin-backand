066f3ebfd96efe20ba9724be4e6db89359151a2b
yuntian <yuntian@tsign.cn>
2025-02-13 10:05:06 +0800

    feat: 脚本

--- prisma/init.sql
+++ prisma/init.sql
@@ -6,3 +6,5 @@ INSERT INTO `role`(`name`, `code`) VALUES
 ('普通用户', 3),
 ('访客用户', 4)
 ;
+
+insert into `user`(`name`, `password`, `roleId`) values('admin','e1ea532cc067acedc149d39a87716f3f', 1) ON DUPLICATE KEY UPDATE roleId=roleId;
